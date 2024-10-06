7109f84b6c2332edc955f86aed66aa69979b59e4
“Simone <simoneruberto@outlook.it>
2024-10-06 13:58:43 +0200

    add systemd service start command to setup script

--- deployment/setup_service.sh
+++ deployment/setup_service.sh
@@ -6,5 +6,4 @@ sudo cp -f Telegram-Member-Booster.timer /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo systemctl enable Telegram-Member-Booster.timer
-
 sudo systemctl start Telegram-Member-Booster.timer
